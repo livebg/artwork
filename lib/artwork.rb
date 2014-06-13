787a5da4bc13dbf344490a288f269dd3872230c0
Dimitar Dimitrov <me@ddimitrov.name>
2014-06-13 16:54:52 +0300

    Allow Artwork to be required without loading Rails
    Useful for the tests.

--- lib/artwork.rb
+++ lib/artwork.rb
@@ -3,7 +3,7 @@ require 'artwork/configuration'
 require 'artwork/model'
 require 'artwork/view'
 require 'artwork/controller'
-require 'artwork/engine' if Rails.const_defined?(:Engine)
+require 'artwork/engine' if Object.const_defined?(:Rails) and Rails.const_defined?(:Engine)
 
 module Artwork
   extend Configuration
