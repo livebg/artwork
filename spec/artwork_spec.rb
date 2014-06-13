233bff354c04afd4d39cd4e6b7ab8f80d1cab0d5
Dimitar Dimitrov <me@ddimitrov.name>
2014-06-13 17:04:56 +0300

    Specs for the Artwork interface

---
+++ spec/artwork_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe Artwork do
+  it 'responds to root_path' do
+    expect(Artwork).to respond_to(:root_path)
+  end
+
+  it 'responds to configuration methods' do
+    expect(Artwork).to respond_to(:configure_for)
+    expect(Artwork).to respond_to(:reset_configuration)
+    expect(Artwork).to respond_to(:default_resolution)
+    expect(Artwork).to respond_to(:default_resolution=)
+    expect(Artwork).to respond_to(:supported_resolutions_list)
+    expect(Artwork).to respond_to(:supported_resolutions_list=)
+    expect(Artwork).to respond_to(:load_2x_images?)
+    expect(Artwork).to respond_to(:current_resolution)
+  end
+end
